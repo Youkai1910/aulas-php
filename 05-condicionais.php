622f8bcedea5572d1118907c85abdc75e9210fc2
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-04 09:05:52 -0300

    Exemplos de CSS html  JS PHP  em Concdicionais Simples e Composta

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -1,12 +1,55 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Condicionais</title>
+    <style>
+        .comprar { color: red;}
+        .normal { color: blue;}
+    </style>
 </head>
+
 <body>
     <h1>Trabalahando com estruturas condicionais</h1>
     <hr>
+    <!-- Lembrete: ao usar condicionais, muitas vezes também usaremos operadores relacionais:
+     <, <=, >=, ==, !=, ===, -->
+    <h2>Condicional SIMPLES: <code>if</code></h2>
+    <?php
+    $numero = 50;
+
+    //Estrututra tradicional (comando, Parenteses, chaves)
+    if ($numero > 10) {
+        echo "<p>$numero é maior que 10.</p>";
+    }
+
+    //Estrututra abreviada (sem chaves)
+    if ($numero > 10) echo "<p>$numero é maior que 10.</p>" ;
+
+    //Estrututra alternativa (sem chaves, com : e endif)
+    if($numero > 10):
+        echo "<p>$numero é maior que 10.</p>";
+    endif;
+    ?>
+    <hr>
+    <h2>Condicional Composta: <code>if/else</code></h2>
+    <?php 
+    $produto = "Ultrabook";
+    $qtdEmEstoque = 30;
+    $qtdCritica = 5;
+     ?>
+     <h3><?= $produto ?></h3>
+     <p><b>Quantidade em estoque:</b><?= $qtdCritica ?></p>
+
+     <?php
+     if ($qtdEmEstoque < $qtdCritica){
+        echo "<p class=\"comprar\">É necessário comprar/repor</p>";
+     }else {
+        echo "<p class= 'normal'>Estoque normal.</p>";
+     }
+     ?>
 </body>
+
 </html>
