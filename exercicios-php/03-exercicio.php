94cdad05471c434187a37b0093975920f2993626
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-05 10:57:14 -0300

    Exemplo com CSS Aplicado

--- exercicios-php/03-exercicio.php
+++ exercicios-php/03-exercicio.php
@@ -1,14 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>03-Exercicio</title>
+    <style>
+        h1 {
+            color: white;
+            background-color: black;
+        }
+
+        h2 {
+            color: white;
+            background-color: black;
+        }
+
+        .container {
+            background-color: black;
+            color: white;
+        }
+    </style>
 </head>
+
 <body>
     <h1>PHP com HTML</h1>
     <hr>
-<!-- Nele, desenvolva um programa que determine o valor do ingresso de um show.
+    <!-- Nele, desenvolva um programa que determine o valor do ingresso de um show.
 Considere os seguintes critérios:
 Para idade abaixo de 12 anos, o ingresso custará R$ 25,00 (Infantil)
 Quando a idade for menor que 60 anos, o ingresso custará R$ 40,00 (Adulto).
@@ -22,24 +40,27 @@ Importante: se necessário, utilize a sintaxe alternativa do if, intercalando PH
 Utilize classes CSS diferentes para cada categoria (por exemplo, alterando a cor de fundo, a borda ou outro estilo visual). 
 A classe CSS deve ser definida pelo PHP e aplicada ao elemento HTML correspondente.
 🔥Desafio 2: existe um recurso do PHP que permite pegar um valor e formatá-lo com símbolo de moeda, separador de milhar e de casas decimais. Descubra qual é o aplique ao valor do ingresso. -->
-<?php $idade = 65;
-if ($idade < 12):
-    $categoria = "Infantil";
-    $valor = 25.00;
+    <?php $idade = 12;
+    if ($idade < 12):
+        $categoria = "Infantil";
+        $valor = 25.00;
     elseif ($idade < 60):
-    $categoria = "Adulto";
-    $valor = 40.00;
+        $categoria = "Adulto";
+        $valor = 40.00;
     else:
-    $categoria = "Melhor Idade";
-    $valor = 20.00;
+        $categoria = "Melhor Idade";
+        $valor = 20.00;
     endif;
-?>
-<h2>Estadio do Corinthians</h2>
-<ul>
-    <li><b>Idade da Pessoa:</b> <?= $idade; ?></li>
-    <li><b>Categoria do Ingresso:</b> <?= $categoria; ?></li>
-    <li><b>Valor do Ingresso:</b> <?= $valor; ?></li>
-</ul>
-    
+    ?>
+    <h2>Estadio do Corinthians</h2>
+    <div class="container">
+        <ul>
+            <li><b>Idade da Pessoa:</b> <?= $idade; ?></li>
+            <li><b>Categoria do Ingresso:</b> <?= $categoria; ?></li>
+            <li><b>Valor do Ingresso:</b> R$ <?= number_format($valor, 2, ',', '.'); ?></li>
+        </ul>
+    </div>
+
 </body>
+
 </html>
