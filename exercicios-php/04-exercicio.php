9ca2d0f4673dac859418c5593e12a8d95ac6e150
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-10 11:49:25 -0300

    04-exercicios

---
+++ exercicios-php/04-exercicio.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 04</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+<!--
+Nele, programe uma estrutura de dados com 5 nomes de linguagens, cada uma com um identificador numérico e uma pequena descrição.
+Exemplo:
+HTML: Estruturação
+CSS: Estilos
+JS: Comportamentos
+PHP: Back-End
+SQL: Manipulação de dados
+Em seguida, use loop para iterar sobre esta estrutura de dados e exibir todos os dados na página usando tags básicas, como h2, p, b, ul e li.-->
+<div class="container">
+<h1>Estrutura de Dados</h1>
+<hr>
+<?php 
+$linguagens = [
+    [
+        "nome" => "HTML",
+        "indentificador" => 1,
+        "descrição" => "Estruturação"
+    ],
+    [
+       "nome" => "CSS",
+        "indentificador" => 2,
+        "descrição" => "Estilos" 
+    ],
+    [
+        "nome" => "JS",
+        "indentificador" => 3,
+        "descrição" => "Comportamentos"
+    ],
+    [
+        "nome" => "PHP",
+        "indentificador" => 4,
+        "descrição" => "Back-End"
+    ],
+    [
+        "nome" => "SQL",
+        "indentificador" => 1,
+        "descrição" => "Manipulação de dados"
+    ]
+];
+?>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    
+</body>
+</html>
