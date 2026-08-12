670d5a980cfdea7830dddaae1dd882bd5345d8ca
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-12 11:50:40 -0300

    05-Exercicios funções do PHP

---
+++ exercicios-php/05-exercicio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios-05</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+ <!--Nele, crie três variáveis para representar as notas de um aluno.
+ Crie duas funções: uma para os processos de cálculo da média, e outra para os processos de verificação da situação do aluno.
+ Chamando as funções, passando os dados para elas e obtendo seus retornos, calcule a média destas notas e verifique a situação do aluno (aprovado ou reprovado). O critério para ser aprovado é ter média a partir de 7.
+ Mostre no HTML usando as tags de sua preferência.
+ Desafios:
+Usando o Bootstrap, formate em vermelho se o aluno estiver reprovado, e em verde se estiver aprovado.
+Adapte o exercício para que funcione com uma lista de 5 alunos, com suas respectivas 3 notas. -->
+
+<div class="container">
+    <h1>Exercicios de funções do PHP</h1>
+
+    <hr>
+   <?php 
+    
+    ?>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
