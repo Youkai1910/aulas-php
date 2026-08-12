6b89ec5e8ab5990691f665647fcc7b6567db69a7
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-12 09:52:35 -0300

    Exercicios Corrigido

--- exercicios-php/04-exercicio.php
+++ exercicios-php/04-exercicio.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 04</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-   <style>
-</style>
+
 </head>
+
 <body>
-<!--
+    <!--
 Nele, programe uma estrutura de dados com 5 nomes de linguagens, cada uma com um identificador numérico e uma pequena descrição.
 Exemplo:
 HTML: Estruturação
@@ -19,87 +20,72 @@ JS: Comportamentos
 PHP: Back-End
 SQL: Manipulação de dados
 Em seguida, use loop para iterar sobre esta estrutura de dados e exibir todos os dados na página usando tags básicas, como h2, p, b, ul e li.-->
-<div class="container">
-<h1>Estrutura de Dados</h1>
-<hr>
-<?php 
-$linguagens = [
-    [
-        "nome" => "HTML",
-        "indentificador" => 1,
-        "descrição" => "Estruturação"
-    ],
-    [
-       "nome" => "CSS",
-        "indentificador" => 2,
-        "descrição" => "Estilos" 
-    ],
-    [
-        "nome" => "JS",
-        "indentificador" => 3,
-        "descrição" => "Comportamentos"
-    ],
-    [
-        "nome" => "PHP",
-        "indentificador" => 4,
-        "descrição" => "Back-End"
-    ],
-    [
-        "nome" => "SQL",
-        "indentificador" => 5,
-        "descrição" => "Manipulação de dados"
-    ]
-];?> 
+    <div class="container">
+        <h1>Estrutura de Dados</h1>
+        <hr>
+        <?php
+        $linguagens = [
+            [
+                "nome" => "HTML",
+                "indentificador" => 1,
+                "descrição" => "Estruturação"
+            ],
+            [
+                "nome" => "CSS",
+                "indentificador" => 2,
+                "descrição" => "Estilos"
+            ],
+            [
+                "nome" => "JS",
+                "indentificador" => 3,
+                "descrição" => "Comportamentos"
+            ],
+            [
+                "nome" => "PHP",
+                "indentificador" => 4,
+                "descrição" => "Back-End"
+            ],
+            [
+                "nome" => "SQL",
+                "indentificador" => 5,
+                "descrição" => "Manipulação de dados"
+            ]
+        ]; ?>
 
-<?php foreach ($linguagens as $linguagem): ?>
-    <h2><?= $linguagem['nome'] ?></h2>
-    <p>Descrição: <?= $linguagem['descrição'] ?></p>
-    
-    <ul>
-        <li><b>ID:</b> <?= $linguagem['indentificador'] ?></li>
-    </ul>
-<?php endforeach; ?>
+        <?php foreach ($linguagens as $linguagem): ?>
+            <h2><?= $linguagem['nome'] ?></h2>
+            <p>Descrição: <?= $linguagem['descrição'] ?></p>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Linguagem</th>
-      <th scope="col">Descrição</th>
-      
-    </tr>
-  </thead>
-  <tbody>
- <tr>
-      <th scope="row">1</th>
-      <td>HTML</td>
-      <td>Estruturação</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>CSS</td>
-      <td>Estilos</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>JS</td>
-      <td>Comportamentos</td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>PHP</td>
-      <td>Back-End</td>
-    </tr>
-    <tr>
-      <th scope="row">5</th>
-      <td>SQL</td>
-      <td>Manipulação</td>
-    </tr>
-  </tbody> 
-</table>
-</div>
+            <ul>
+                <li><b>ID:</b> <?= $linguagem['indentificador'] ?></li>
+            </ul>
+        <?php endforeach; ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    
+        <hr>
+
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Linguagem</th>
+                    <th>Descrição</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($linguagens as $linguagem): ?>
+                    <tr>
+                        <td><?= $linguagem['indentificador'] ?></td>
+                        <td><?= $linguagem['nome'] ?></td>
+                        <td><?= $linguagem['descrição'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
