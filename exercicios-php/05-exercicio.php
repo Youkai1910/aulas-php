830124eb0fc0782734e916e2c3d2a48cdb361f50
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-13 19:01:26 -0300

    o problema resolvido

--- exercicios-php/05-exercicio.php
+++ exercicios-php/05-exercicio.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicios-05</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <style>
+     tr:nth-child(1) td { background-color: greenyellow; color: black;}
+     tr:nth-child(2) td { background-color: whitesmoke; color: black;}
+     tr:nth-child(3) td { background-color: yellow; color: black;}
+     tr:nth-child(4) td { background-color: greenyellow; color: black;}
+     tr:nth-child(5) td { background-color: whitesmoke; color: black;}
+    </style>
 </head>
 
 <body>
