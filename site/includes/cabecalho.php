9e99cafa24855c2020adc0870682e30e437403ea
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-19 11:39:19 -0300

    pasta site

---
+++ site/includes/cabecalho.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site usando PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Site com PHP</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="curso.php">Cursos</a>
+            <a href="duvidas.php">Duvidas</a>
+        </nav>
+    </header>
+    <main>
--- site/index.php
+++ site/index.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site usando PHP</title>
-</head>
-<body>
-    <header>
-        <h1>Site com PHP</h1>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="curso.php">Cursos</a>
-            <a href="duvidas.php">Duvidas</a>
-        </nav>
-    </header>
-    <main>
+<?php require "includes/cabecalho.php" ?>
+
         <h2>Bem-vindo ao site de exemplo</h2>
         <p>Esta é a primeira do nosso site</p>
-    </main>
 
-    <footer>
-        <p>Site criado usando PHP</p>
-    </footer>
-</body>
-</html>
+        <?php require "includes/rodape.php" ?>
+    
