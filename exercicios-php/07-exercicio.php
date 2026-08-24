4abffe0f7626ae630a7db38a5ebf83ddb1cde07d
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-24 11:41:17 -0300

    07-exercicio.php

---
+++ exercicios-php/07-exercicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios</title>
+</head>
+<body>
+    <!-- crie um formulário para cadastro (simulação) de produtos com os campos: 
+Nome do produto (input text) 
+Fabricante (select de options com pelo menos 4 nomes de fabricantes)  
+Preço (input number com valor mínimo de 100 e máximo de 10000, além de suporte à 2 casas decimais para os centavos) 
+Quantidade (input number, mínimo 0 e máximo de 1000, sem casas decimais) 
+Em um novo arquivo chamado exercicio07-processamento.php, faça a programação de processamento do formulário considerando o envio/recebimento via POST, filtros de sanitização e validação de todos os campos como obrigatórios. 
+Faça também a exibição dos dados no HTML usando as tags de sua preferência. 
+🔥 Desafio: 
+Calcule o valor total fazendo a multiplicação do preço pela quantidade 
+Mostre o valor total calculado. -->
+
+    <div class="container">
+        <h1>Formulário para Cadastro de Produtos</h1>
+        <hr>
+    </div>
+</body>
+</html>
